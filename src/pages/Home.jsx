bb9ac228c8abe506704b585b75c69125a9e4151f
Luis Cruz <estecruzma12@gmail.com>
2026-04-29 18:43:41 -0500

    Cambio en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,34 +35,28 @@ function Home() {
 
       <section className="features">
 
-        <div className="feature left">
-          <div className="feature-text">
-            <h2>¿Qué hacemos?</h2>
-            <p>
-              Ofrecemos talleres de liderazgo y coaching diseñados para el crecimiento personal,
-              ayudando a las personas a descubrir su potencial y transformar su vida.
-            </p>
-            <img src={Talleres} className="feature-img" />
-          </div>
+        <div className="card">
+          <img src={Talleres} />
+          <h2>¿Qué hacemos?</h2>
+          <p>
+            Ofrecemos talleres de liderazgo y coaching diseñados para el crecimiento personal,
+            ayudando a las personas a descubrir su potencial y transformar su vida.
+          </p>
         </div>
 
-        <div className="feature right">
-          <div className="feature-text">
-            <img src={Talleres} className="feature-img" />
-            <h2>Trabajo en equipo</h2>
-            <p>Desarrolla habilidades para liderar grupos</p>
-          </div>
+        <div className="card">
+          <img src={Talleres} />
+          <h2>Trabajo en equipo</h2>
+          <p>Desarrolla habilidades para liderar grupos</p>
         </div>
 
-        <div className="feature left">
-          <div className="feature-text">
-            <h2>Nosotros</h2>
-            <p>
-              Somos una escuela de liderazgo enfocada en el crecimiento humano.
-              Contamos con coaches certificados y una comunidad en constante expansión.
-            </p>
-            <img src={Talleres} className="feature-img" />
-          </div>
+        <div className="card">
+          <img src={Talleres} />
+          <h2>Nosotros</h2>
+          <p>
+            Somos una escuela de liderazgo enfocada en el crecimiento humano.
+            Contamos con coaches certificados y una comunidad en constante expansión.
+          </p>
         </div>
 
       </section>
